2adc9035b4f76d1d84331375a5519b565c520af8
addinrw <akhul.syaifudin@rumahweb.co.id>
2024-01-13 11:16:31 +0700

    init signin page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  css:[
+    "~/assets/keenicons/duotone/style.css",
+    "~/assets/keenicons/outline/style.css",
+    "~/assets/keenicons/solid/style.css",
+    // "~/assets/sass/element-ui.dark.scss",
+    // "~/assets/sass/plugins",
+    // "~/assets/sass/style",
+    "@/assets/sass/element-ui.dark.scss",
+    "@/assets/sass/plugins.scss",
+    "@/assets/sass/style.scss"
+  ],
+  modules:[
+    '@pinia/nuxt'
+  ],
+  runtimeConfig:{
+    public:{
+      BASE_URL: "http://localhost:3000/"
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-
+definePageMeta({
+  layout: 'auth'
+})
 </script>
-
-<style>
-
-</style>
