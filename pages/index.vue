1734a5326e741c8cd2ac2a9a260903e719c98be2
addinrw <akhul.syaifudin@rumahweb.co.id>
2024-01-13 08:57:55 +0700

    remove app.vue & index firebase

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit necessitatibus nostrum dicta dignissimos tempore ipsa similique exercitationem alias repellendus in. Consectetur officiis beatae nemo consequatur similique saepe nulla distinctio perspiciatis.
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
